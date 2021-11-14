b9e4efd803179f5d8eafc76fa2228259a36677da
murtazaaylak <murtazaaylak@gmail.com>
2021-11-14 22:10:15 +0300

    add: global css file & remove initial files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>SPLIT</h2>
+    <h2>TTER</h2>
+    <card />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Card from './components/Card.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Card,
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
